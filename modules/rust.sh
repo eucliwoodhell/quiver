88e4167a08534b7ab2d0323462477d3655477a25
Paolo Castillo <archercastillo@MacBook-Pro.local>
2026-03-18 19:10:22 -0300

    wip

--- modules/rust.sh
+++ modules/rust.sh
@@ -5,34 +5,32 @@ focus_install() {
 
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-  install_manga_reader
+  log_success "Rust instalado."
+
+  install_plugins
   return 0
 }
 
-install_manga_reader() {
+install_plugins() {
   # https://github.com/mayocream/koharu
   if gum confirm "Instalar Manga Reader?"; then
-    log_info "Instalando Manga Reader..."
     cargo install manga-tui
   fi
 
   if gum confirm "Instalar Ghgrab?"; then
-    log_info "Instalando Ghgrab..."
-    cargo install ghgrab
+    cargo install ghgrab >
   fi
 
   if gum confirm "Instalar Deadbranch?"; then
-    log_info "Instalando Manga Downloader..."
     cargo install deadbranch
   fi
 
   if gum confirm "Instalar flux" then
-    log_info "Instalando flux..."
     cargo install flux-cli
   fi
 
   if gum confirm "Instalar suvadu" then
     cargo install suvadu
-    echo 'eval "$(suv init zsh)"' >> ~/.zshrc && source ~/.zshrc
+    echo 'eval "$(suv init zsh)"' >> ~/.zshrc
   fi
 }
