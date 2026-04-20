2f9cf52e96d32af44830da6a34b5317b16719aaa
Paolo Castillo <archercastillo@MacBook-Pro.local>
2026-04-20 10:06:55 -0400

    chore(nvim): clone repo nvim

--- modules/nvim.sh
+++ modules/nvim.sh
@@ -7,5 +7,19 @@ focus_install() {
 
   link_config "nvim"
 
+  install_nvim_configs
+  return 0
+}
+
+install_nvim_configs() {
+  if gum confirm "Instalar configs de Neovim?"; then
+    log_info "Instalando Neovim configs..."
+    gum spin --spinner dot --title "Neovim configs" -- git clone https://github.com/eucliwoodhell/my-nvim-lua.git ~/.config/nvim
+
+    if gum confirm "Desea utilizar la rama lazy?"; then
+      git -C ~/.config/nvim checkout lazy-config
+    fi
+  fi
+
   return 0
 }
